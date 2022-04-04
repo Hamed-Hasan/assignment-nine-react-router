97a984f34be9f475ba2ddc486ef24d19a4c4fbc0
John Doe <hamedhasan422421@gmail.com>
2022-04-04 09:38:24 +0300

    review section added

---
+++ src/component/CustomersReviews/CustomersReviews.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import useProducts from '../hooks/useProducts';
+import ReviewProductThree from '../ReviewProductThree/ReviewProductThree';
+import './CustomersReviews.css'
+const CustomersReviews = () => {
+    const [products, setProducts] = useProducts();
+    const sliceProducts = products;
+    const result = sliceProducts.slice(0,3)
+    console.log(result)
+    return (
+        <div>
+            <h1 className='text-center text-5xl font-bold py-10'>Customers Reviews  ({result.length})</h1>
+{
+    result.map(product => <ReviewProductThree 
+    key={product.id}
+    product={product}
+    />)
+}
+        </div>
+    );
+};
+
+export default CustomersReviews;
---
+++ src/component/ReviewProductThree/ReviewProductThree.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const ReviewProductThree = ({ product }) => {
+    const {title,description,avatar,avName} = product;
+    
+    return (
+        <div className='px-10 lg:px-60 '>
+            
+
+            <ol className="relative border-l border-gray-200 dark:border-gray-700 ">
+<li className="mb-10 ml-6">
+<span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+<img className="rounded-full shadow-lg" src={avatar} alt="Bonnie image"/>
+</span>
+<div className="justify-between items-center mt-8 p-4 bg-white rounded-lg border border-gray-200 shadow-sm sm:flex dark:bg-gray-700 dark:border-gray-600">
+<time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">just now</time>
+<div className="text-sm font-normal text-gray-500 dark:text-gray-300 "> {title.length > 20 ? title.slice(0, 40) : title}<span className="bg-gray-100 text-gray-800 text-xs font-normal mr-2 px-2.5 py-0.5 rounded dark:bg-gray-600 dark:text-gray-300 ml-4"> USA</span></div>
+</div>
+</li>
+<li className=" ml-6">
+<span className="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+<img className="rounded-full shadow-lg" src={avatar} alt="Thomas Lean image"/>
+</span>
+<div className="p-4 bg-white rounded-lg border border-gray-200 shadow-sm dark:bg-gray-700 dark:border-gray-600">
+<div className="justify-between items-center mb-3 sm:flex">
+<time className="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">2 hours ago</time>
+<div className="text-sm font-normal text-gray-500 lex dark:text-gray-300 text-2xl">{avName} </div>
+</div>
+<div className="p-3 text-xs italic font-normal text-gray-500 bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-600 dark:border-gray-500 dark:text-gray-300">{description}</div>
+
+
+<div className="flex items-center mt-2.5 mb-5">
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <svg className="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+            <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+        </div>
+
+</div>
+</li>
+
+</ol>
+
+
+        </div>
+    );
+};
+
+export default ReviewProductThree;
