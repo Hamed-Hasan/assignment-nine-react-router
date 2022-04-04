64e4bf055dfa97ad3bf5a6790224cd5f04092604
John Doe <hamedhasan422421@gmail.com>
2022-04-04 14:31:24 +0300

    blog page added

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './About'
 const About = () => {
     return (
         <section className="text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Hi There's
-              <br className="hidden lg:inline-block"/>
-            </h1>
-            <p className="mb-8 leading-relaxed">
-I am Hamed Hasan. When I some Computing I agreed to learn Coding . It's Not easy but I Work hard when learning HTML CSS JavaScript and I fill proud happy but when start React Js its to complicated in other language . In this situation I trying to over come to my Weakness</p>
-            <div className="flex justify-center">
-            <Link to='/' type="button" className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-    Back To Home
-    <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-</Link>
-            </div>
-          </div>
+        
+        <div className="p-4 md:w-1/2 w-full ">
+        <div className="h-full bg-black p-8 rounded py-12">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="block w-5 h-5 text-gray-400 mb-4" viewBox="0 0 975.036 975.036">
+            <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+          </svg>
+          <p className="leading-relaxed mb-6 text-white"> When I some Computing I agreed to learn Coding . It's Not easy but I Work hard when learning HTML CSS JavaScript and I fill proud happy but when start React Js its to complicated in other language . In this situation I trying to over come to my Weakness</p>
+          <a className="inline-flex items-center">
+            <span className="flex-grow flex flex-col pl-4">
+              <span className="title-font font-medium text-white">I am Hamed Hasan.</span>
+              <span className="text-gray-500 text-sm">Front End Developer</span>
+            </span>
+          </a>
+        </div>
+      </div>
+ 
+
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <img className="object-cover object-center rounded" alt="hero" src="https://www.propatel.com/wp-content/uploads/2019/09/coding-and-testing-programming-of-software.jpg"/>
           </div>
